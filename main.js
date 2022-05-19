924055df3b09b8bb51501a4f2fcda7c6170700c9
Santosh Katowal <101291533+SANTOSHKATOWAL@users.noreply.github.com>
2022-05-19 18:41:24 +0530

    collegewebfull

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    document.querySelector('.cont_principal').className= "cont_principal cont_error_active";  
+      
+    }
+
+
+    var lvalue= document.getElementById("rectangle").innerHTML;
+var textLength = lvalue.length();
